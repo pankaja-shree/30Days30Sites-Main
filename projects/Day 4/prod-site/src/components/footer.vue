00e2caaebde73e85b209403b4f3ae235de9d8a70
Pankajashree <pankajashree@gmail.com>
2017-08-01 08:15:05 +0530

    add footer component

---
+++ projects/Day 4/prod-site/src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+<footer>
+        &copy; Design using VueJS and Vue-material by Pankajashree R, 2017
+</footer>
+</template>
+<style>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  padding: 1%;
+  background-color: #bcd;
+  text-align: center;
+}
+</style>
