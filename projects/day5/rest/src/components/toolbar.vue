20c792511a48b94a1cda03a60826761ae308d1f4
Pankajashree <pankajashree@gmail.com>
2017-08-04 22:23:12 +0530

    add toolbar component

---
+++ projects/day5/rest/src/components/toolbar.vue
@@ -0,0 +1,19 @@
+<template>
+<md-toolbar>
+    <md-icon>local_dining</md-icon><h1 class="md-title" style="flex: 1">&nbsp; Yumm</h1>
+    <md-button class="md-icon-button">
+      <md-icon>info</md-icon>
+    </md-button>
+    <md-button class="md-icon-button">
+      <md-icon>call</md-icon>
+    </md-button>
+    <md-button class="md-icon-button">
+      <md-icon>directions</md-icon>
+    </md-button>
+    <md-button class="md-icon-button">
+      <md-icon>account_circle</md-icon>
+    </md-button>
+  </md-toolbar>
+  </template>
+
+  <style src=".../node_modules/vue-material/dist/vue-material.css"></style>
