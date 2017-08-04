9d4aaba3ce09ba3189e481c37ede88169e82c686
Pankajashree <pankajashree@gmail.com>
2017-08-04 20:25:35 +0530

    Add vue project scaffold

---
+++ projects/day5/rest/src/components/footer.vue
@@ -0,0 +1,13 @@
+<template>
+<footer>
+        &copy; Designed using VueJS and Vue-material by Pankajashree R.
+</footer>
+</template>
+<style>
+footer {
+    flex: 0 0 50px; /*don't grow or shrink - just stay at a height of 50px.*/
+    padding: 10px;
+    color: #fff;
+    background-color: #3F51B5;
+  }
+</style>
