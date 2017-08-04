f46c333cded2778a76974975edf0cca59f7770a8
Pankajashree <pankajashree@gmail.com>
2017-08-04 22:26:01 +0530

    Add tooltip to icons

--- projects/day5/rest/src/components/toolbar.vue
+++ projects/day5/rest/src/components/toolbar.vue
@@ -3,15 +3,19 @@
     <md-icon>local_dining</md-icon><h1 class="md-title" style="flex: 1">&nbsp; Yumm</h1>
     <md-button class="md-icon-button">
       <md-icon>info</md-icon>
+      <md-tooltip>About</md-tooltip>
     </md-button>
     <md-button class="md-icon-button">
       <md-icon>call</md-icon>
+      <md-tooltip>Call</md-tooltip>
     </md-button>
     <md-button class="md-icon-button">
       <md-icon>directions</md-icon>
+      <md-tooltip>Get Directions</md-tooltip>
     </md-button>
     <md-button class="md-icon-button">
       <md-icon>account_circle</md-icon>
+      <md-tooltip>Sign in</md-tooltip>
     </md-button>
   </md-toolbar>
   </template>
