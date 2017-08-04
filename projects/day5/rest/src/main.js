126a186c0d5997f748be34431112829d6bcd7fc8
Pankajashree <pankajashree@gmail.com>
2017-08-04 21:17:52 +0530

    add custom themes

--- projects/day5/rest/src/main.js
+++ projects/day5/rest/src/main.js
@@ -1,5 +1,26 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
 import App from './App.vue'
+
+Vue.use(VueMaterial)
+
+Vue.material.registerTheme({
+  default: {
+    primary: {
+      color: 'light-green',
+      hue: 700
+    },
+    accent: 'red'
+  },
+  teal: {
+    primary: 'blue',
+    accent: 'pink'
+  },
+  purple: {
+    primary: 'purple',
+    accent: 'orange'
+  }
+})
 
 new Vue({
   el: '#app',
