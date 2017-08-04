68e4be652a146302734cc92aedd32aa889dcb1a8
Pankajashree <pankajashree@gmail.com>
2017-08-04 21:53:16 +0530

    add tabs

--- projects/day5/rest/src/App.vue
+++ projects/day5/rest/src/App.vue
@@ -1,30 +1,41 @@
 <template>
   <div id="app">
     <md-toolbar>
-    <h1 class="md-title" style="flex: 1">Yumm</h1>
+    <md-icon>local_dining</md-icon><h1 class="md-title" style="flex: 1">&nbsp; Yumm</h1>
     <md-button class="md-icon-button">
-      <md-icon>more_vert</md-icon>
+      <md-icon>info</md-icon>
+    </md-button>
+    <md-button class="md-icon-button">
+      <md-icon>directions</md-icon>
+    </md-button>
+    <md-button class="md-icon-button">
+      <md-icon>call</md-icon>
     </md-button>
   </md-toolbar>
 
-  <md-tabs md-fixed md-purple>
-  <md-tab id="starters" md-label="Starters">
+  <md-tabs md-fixed>
+  <md-tab id="starters" md-label="Starters" md-icon="local_pizza">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
   </md-tab>
 
-  <md-tab id="lunch" md-label="Lunch">
+  <md-tab id="lunch" md-label="Lunch" md-icon="restaurant">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
   </md-tab>
 
-  <md-tab id="dinner" md-label="Dinner">
+  <md-tab id="dinner" md-label="Dinner" md-icon="restaurant_menu">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
   </md-tab>
 
-  <md-tab id="drinks" md-label="Beverages">
+  <md-tab id="desserts" md-label="Desserts" md-icon="cake">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
+  </md-tab>
+
+  <md-tab id="drinks" md-label="Beverages" md-icon="local_bar">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas.</p>
   </md-tab>
 </md-tabs>
+
 
   </div>
 </template>
@@ -34,7 +45,21 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      'starters': [
+
+      ],
+      'lunch':[
+
+      ],
+      'dinner':[
+
+      ],
+      'desserts':[
+
+      ],
+      'beverages':[
+        
+      ]
     }
   }
 }
