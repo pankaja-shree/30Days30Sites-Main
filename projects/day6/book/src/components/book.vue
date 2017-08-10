30d76a7d34d3bb113b08daf163a5d86371faa7ab
Pankajashree <pankajashree@gmail.com>
2017-08-10 22:10:11 +0530

    add book component

---
+++ projects/day6/book/src/components/book.vue
@@ -0,0 +1,38 @@
+<template>
+<!-- Book -->
+	<section class="book" id="book">
+		<div class="book-bg">
+			<div class="container section-wrapper">
+				<div class="row">
+					<div class="col-sm-6">
+						<h2 class="section">
+							About The Book
+						</h2>
+						<p class="light">
+							This lovely, well-written book is concerned foremost with creating typography and is essential for professionals who regularly work for clients.
+						</p>
+						<p class="body">
+							Ankkurissa seuratessa ai on et porstuasta en pirullisen. Ei ne ristiin paassyt he eikohain isomman tulossa uudesta. Nyt ota jattaa viinan edessa kai. <strong>Your Culture is your brand</strong> .Kuunnella ainaisena se kajuttaan ja asiakseen. Voi sylkea kay jos auttaa saalla nuo siella saukko sisaan. Kuin no koko ihan joka te mina he ne.
+						</p>
+						<p class="body">
+							uoksette ota vavahtaen jaa tee <strong>kaantynyt</strong> mimmoinen. Paahasi et talloin ja ne uusilla. Paljo milla uhria ja ja onkin. Jotakin on jo karille jaa tee kaanty ok done.
+						</p>
+					</div>
+				</div>
+			</div> <!-- .container -->
+		</div> <!-- /.book-bg -->
+	</section> <!-- #book --> 
+	<section class="cta-1 container-fluid">
+		A few hundred years of type and <strong class="regular">typography</strong> have <br> established <strong class="regular">rules</strong> that only a fool would ignore.
+	</section> <!-- /.cta-1 -->
+</template>
+
+<script>
+export default {
+  name: 'book',
+  data () {
+    return {
+    }
+  }
+}
+</script>
