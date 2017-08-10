0dbd08a2884ce7516713f6d4504d35608b09a892
Pankajashree <pankajashree@gmail.com>
2017-08-10 21:45:20 +0530

    add links to bootstrap

--- projects/day6/book/src/main.js
+++ projects/day6/book/src/main.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+//Bootstrap vue dependencies
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
+
 import App from './App.vue'
 
 new Vue({
