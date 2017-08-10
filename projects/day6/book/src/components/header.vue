2b1d9ef8fb38109c3b7a4d2d971edc792c467cfe
Pankajashree <pankajashree@gmail.com>
2017-08-10 21:45:44 +0530

    create header component

---
+++ projects/day6/book/src/components/header.vue
@@ -0,0 +1,36 @@
+<template>
+<!-- Home -->
+	<section class="home" id="home">
+		<div class="home-bg">
+			<div class="container-fluid">
+				 <div class="row">
+					<div class="col-xs-12 col-sm-5 col-sm-offset-6 col-md-5">
+						<div class="header-wrapper">
+							<h1 class="header-title">Notebook Mockup</h1>
+							<p class="header-sub">
+								Review of typefaces with examples.
+							</p>
+							<p class="description">
+								Our all time best selling book is now available in a revised and expanded second edition. Notebook Mockup Vol2 is the definitive guide to using type in visual communication, from the printed page to PC screen.
+							</p>
+							<a class="btn btn-default btn-lg bttn" href="#" role="button">About The Book</a>
+							<a class="btn btn-default btn-lg red" href="#" role="button">Purchase eBook $99</a>
+						</div> <!-- /.header-wrapper -->
+					</div>	<!-- .col-sm-5 -->
+				</div> <!-- .row -->
+			</div> <!-- /.container-fluid -->
+		</div> <!-- .home-bg -->
+	</section> <!-- #home -->
+    </template>
+
+    <script>
+export default {
+  name: 'header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
