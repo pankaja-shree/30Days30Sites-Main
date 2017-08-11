25480ee08705681c32c4b56b0b5135f75d657180
Pankajashree <pankajashree@gmail.com>
2017-08-11 21:44:48 +0530

    remove book component from header

--- projects/day6/book/src/components/header.vue
+++ projects/day6/book/src/components/header.vue
@@ -19,28 +19,6 @@
 					</div>	<!-- .col-sm-5 -->
 				</div> <!-- .row -->
 			</div> <!-- /.container-fluid -->
-				<section class="book" id="book">
-		<div class="book-bg">
-			<div class="container section-wrapper">
-				<div class="row">
-					<div class="col-sm-6">
-						<h2 class="section">
-							About The Book
-						</h2>
-						<p class="light">
-							Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-						</p>
-						<p class="body">
-							Ankkurissa seuratessa ai on et porstuasta en pirullisen. Ei ne ristiin paassyt he eikohain isomman tulossa uudesta. Nyt ota jattaa viinan edessa kai. <strong>Your Culture is your brand</strong> .Kuunnella ainaisena se kajuttaan ja asiakseen. Voi sylkea kay jos auttaa saalla nuo siella saukko sisaan. Kuin no koko ihan joka te mina he ne.
-						</p>
-						<p class="body">
-							uoksette ota vavahtaen jaa tee <strong>kaantynyt</strong> mimmoinen. Paahasi et talloin ja ne uusilla. Paljo milla uhria ja ja onkin. Jotakin on jo karille jaa tee kaanty ok done.
-						</p>
-					</div>
-				</div>
-			</div> <!-- .container -->
-		</div> <!-- /.book-bg -->
-	</section> <!-- #book --> 
 	<section class="cta-1 container-fluid">
 	<blockquote class="blockquote">
 		<p class="mb-0">"A few hundred years of type and <strong class="regular">typography</strong> have <br> established <strong class="regular">rules</strong> that only a fool would ignore."</p>
@@ -51,15 +29,11 @@
     </template>
 
     <script>
-		import book from './book.vue'
 export default {
   name: 'header',
   data () {
     return {
     }
-  },
-	components: {
-    'app-book': book
   }
 }
 </script>
