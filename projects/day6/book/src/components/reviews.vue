e2d7511d43429a234dc0f05f2edc78fc45b42643
Pankajashree <pankajashree@gmail.com>
2017-08-11 22:58:51 +0530

    add reviews component

---
+++ projects/day6/book/src/components/reviews.vue
@@ -0,0 +1,24 @@
+<template>
+	<section class="cta-1 container-fluid">
+	<blockquote class="blockquote">
+		<p class="mb-0"><strong class="regular">"Voi sylkea kay jos auttaa saalla nuo siella saukko sisaan. Kuin no koko ihan joka te mina he ne."</strong> <br> - John Doe</p>
+	</blockquote>
+    <blockquote class="blockquote">
+		<p class="mb-0"><strong class="regular">"Voi sylkea kay jos auttaa saalla nuo siella saukko sisaan. Kuin no koko ihan joka te mina he ne."</strong> <br> - John Doe</p>
+	</blockquote>
+    <blockquote class="blockquote">
+		<p class="mb-0"><strong class="regular">"Voi sylkea kay jos auttaa saalla nuo siella saukko sisaan. Kuin no koko ihan joka te mina he ne."</strong> <br> - John Doe</p>
+	</blockquote>
+	</section> <!-- /.cta-1 -->
+    </template>
+
+    <script>
+export default {
+  name: 'reviews',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
