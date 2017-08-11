70bddff6c21aa73be9c402ca62786df1b64a1f6e
Pankajashree <pankajashree@gmail.com>
2017-08-11 22:40:29 +0530

    remove blockquote

--- projects/day6/book/src/components/header.vue
+++ projects/day6/book/src/components/header.vue
@@ -1,6 +1,7 @@
 <template>
+<div id="header">
 <!-- Home -->
-	<section class="home" id="home">
+	<section class="home">
 		<div class="home-bg">
 			<div class="container-fluid">
 				 <div class="row">
@@ -8,7 +9,7 @@
 						<div class="header-wrapper">
 							<h1 class="header-title">Book title</h1>
 							<p class="header-sub">
-								Book subtitle; Author name.
+								Book subtitle.
 							</p>
 							<p class="description">
 								Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
@@ -19,15 +20,10 @@
 					</div>	<!-- .col-sm-5 -->
 				</div> <!-- .row -->
 			</div> <!-- /.container-fluid -->
-	<section class="cta-1 container-fluid">
-	<blockquote class="blockquote">
-		<p class="mb-0">"A few hundred years of type and <strong class="regular">typography</strong> have <br> established <strong class="regular">rules</strong> that only a fool would ignore."</p>
-	</blockquote>
-	</section> <!-- /.cta-1 -->
 		</div> <!-- .home-bg -->
-	</section> <!-- #home -->
+	</section>
+	</div>
     </template>
-
     <script>
 export default {
   name: 'header',
