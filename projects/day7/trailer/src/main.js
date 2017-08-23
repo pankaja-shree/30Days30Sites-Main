3d3b4b453e75363e6382873075e3fcdb5bfccf56
Pankajashree <pankajashree@gmail.com>
2017-08-23 22:26:15 +0530

    add bootstrap vue carousel component

--- projects/day7/trailer/src/main.js
+++ projects/day7/trailer/src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
-
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { bCarousel, bCarouselSlide } from 'bootstrap-vue/lib/components'
 import App from './App.vue'
 
+Vue.use(BootstrapVue);
+
+new Vue({
+  components: {
+    bCarousel,
+    bCarouselSlide
+  }
+})
 new Vue({
   el: '#app',
   render: h => h(App)
