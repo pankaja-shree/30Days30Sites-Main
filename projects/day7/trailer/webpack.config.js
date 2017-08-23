da33090d936dd544b4aa37f6a2624cccd54a61d1
Pankajashree <pankajashree@gmail.com>
2017-08-23 19:09:52 +0530

    remove vue-icon

--- projects/day7/trailer/webpack.config.js
+++ projects/day7/trailer/webpack.config.js
@@ -34,14 +34,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /vue-icons/,
-        loader: 'callback-loader',
-        enforce: 'post',
-        options: {
-          plugins: require('vue-icons/icon-loader')(['material-add', 'material-remove'])
-        }
       }
     ]
   },
